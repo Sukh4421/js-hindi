1639566c36b4c4fd1493906993d5a4df3982aec5
Sukh4421 <sukhbir0427@gmail.com>
2025-08-28 19:14:15 +0000

    DataTypes

---
+++ 01_basics/01_Datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // to treat the js code as newer version
+
+/*
+JavaScript is a dynamically typed language. This means you don't have to specify the data type of a variable when you declare it. It also means that data types are automatically converted as-needed during script execution.
+
+Seven data types that are primitives:
+
+1. Boolean. true and false.
+2. null. A special keyword denoting a null value. (Because JavaScript is case-sensitive, null is not the same as Null, NULL, or any other variant.)
+3. undefined. A top-level property whose value is not defined.
+4. Number. An integer or floating point number. For example: 42 or 3.14159.
+5. BigInt. An integer with arbitrary precision. For example: 9007199254740992n.
+6. String. A sequence of characters that represent a text value. For example: "Howdy".
+7 Symbol. A data type whose instances are unique and immutable.
+
+
+
+
+
+
+Non primitive Datatypes:
+1. Arrays
+2. Objects
+3. Functions
+*/
+
+let x = 5;
+let y=x+2;
+console.log(x);
+console.log(y);
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,37 @@
+/*
+Variables are basically a container for storing data.
+
+Variables can be declared:-
+1. Automatically
+2. Using Var
+3. Using let
+4. Using const
+
+Automatically declared variables are undefined and doesn't use any Keyword.
+
+VAriables declared using Var
+have been used since 1995, hence it is very rarely used nowadays, preferably it is used for the older browsers only.
+ It doesnot have any block scope.
+ can be used before declaration.
+  It supports Hoisting.
+   it can be redeclared and reasigned at any point.
+
+Hoisting is refered to the behavior of js to move all the declarations to the top of the code.
+
+Variables declared using let
+Added to the js in 2015.
+has block scope.
+cannot be redeclared in the same scope.
+can be re-assigned.
+doesnot support hoisting.
+
+Variables declared using Const
+added to the js in 2015 with let.
+has block scope.
+values can not be reassigned.
+cannot be redeclared.
+doesnot support hoisting.
+IN CASE OF const ARRAY :- can Change/Add the elements but cannot reassign the array 
+IN CASE OF const OBJECT :- can Change/Add the properties but cannot reassign the object
+
+*/
